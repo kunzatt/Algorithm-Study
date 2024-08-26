d9c379d6f7ecd66c35cb225c48cfa768e50fa734
강지은 <112616257+antisdun@users.noreply.github.com>
2024-08-26 14:09:28 +0900

    [BOJ]  N과 M(2) / 실버3 / 10분
    https://www.acmicpc.net/problem/15650

---
+++ 강지은/baekjoon/Main_15650.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_15650 {
+
+	static int N,M;
+	static List<Integer> nums = new ArrayList<>();
+	static boolean[] visit;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		visit = new boolean[N+1];
+		
+		nPr(1, 0);
+		System.out.println(sb.toString());
+	}
+
+	private static void nPr(int num, int cnt) {
+		if (cnt == M) {
+			for (int i = 0; i < nums.size(); i++) {
+				sb.append(nums.get(i)+" ");
+        // 한 줄에 M개씩 출력
+				if ((i+1)%M==0)
+					sb.append("\n");
+			}
+			return;
+		}
+		
+		for (int i = num; i < N+1; i++) {
+			if (visit[i])
+				continue;
+			
+			nums.add(i);
+			visit[i] = true;
+			nPr(++num, ++cnt);
+			visit[i] = false;
+			nums.remove(--cnt);
+		}
+		
+	}
+
+}
