c3ed6d2f1f337b707e70aa07960a8d7568b26e97
Yong <98104967+kunzatt@users.noreply.github.com>
2025-01-13 22:55:25 +0900

    [BOJ] 어두운 굴다리 / 실버 4 / 35분
    https://www.acmicpc.net/problem/17266

--- 김용명/Baekjoon/Main_17266.java
+++ 김용명/Baekjoon/Main_17266.java
@@ -2,26 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main_17266 {
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        
+
         int[] lights = new int[M];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++) {
             lights[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         System.out.println(binarySearch(N, lights));
     }
-    
+
     static int binarySearch(int N, int[] lights) {
         int left = 1, right = N;
-        
+
         while(left <= right) {
             int mid = (left + right) / 2;
             if(canCover(N, lights, mid)) {
@@ -32,15 +32,15 @@ public class Main_17266 {
         }
         return left;
     }
-    
+
     static boolean canCover(int N, int[] lights, int height) {
         int covered = 0;
-        
+
         for(int pos : lights) {
             if(pos - height > covered) return false;
             covered = pos + height;
         }
-        
+
         return covered >= N;
     }
 }
