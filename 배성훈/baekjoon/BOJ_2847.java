567e146534ffd4b19842b5e5ca55ca53b850217a
Seonghun, Bae <bsh7937@naver.com>
2025-02-19 00:38:37 +0900

    [BOJ] 게임을 만든 동준이 / 실버 4 / 10분
    https://www.acmicpc.net/problem/2847

---
+++ 배성훈/baekjoon/BOJ_2847.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class BOJ_2847 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            sc.nextLine();
+        }
+        int ans=0;
+        for(int i=n-1;i>0;i--){
+            while(arr[i]<=arr[i-1]) {
+                arr[i-1]--;
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
