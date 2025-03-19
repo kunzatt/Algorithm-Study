09934004fbfea481573118be40d78b99633cad1a
dlacowns21 <dlacowns21@naver.com>
2025-03-19 20:58:53 +0900

    [BOJ] 경로찾기 / 실버1 / 30분

---
+++ 임채준/class/Class3/Boj_11403_경로찾기.java
@@ -0,0 +1,46 @@
+package class3;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_11403_경로찾기 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] map = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int k = 0; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(map[i][k] == 1 && map[k][j] == 1) {
+						map[i][j] = 1;
+					}
+				}
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				sb.append(map[i][j]);
+				if(j < N - 1) sb.append(" ");				
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
