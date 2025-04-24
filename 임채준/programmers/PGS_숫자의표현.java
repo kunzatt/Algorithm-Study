e26450b0553861d598f6aa34d9eed372c57334c7
dlacowns21 <dlacowns21@naver.com>
2025-04-25 08:49:26 +0900

    [PGS] 다음 큰 숫자 / LV2 / 5분

---
+++ 임채준/programmers/PGS_숫자의표현.java
@@ -0,0 +1,21 @@
+public class PGS_숫자의표현 {
+
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += j;
+                if(sum == n) {
+                    answer++;
+                    break;
+                } else if(sum > n){
+                    break;
+                }
+            }
+        }
+        answer++;
+        return answer;
+    }
+
+}
